7a39138685f03855fd1329fd4a3aad09f55348ee
rivaldofez <rivaldofez@gmail.com>
2023-12-16 01:18:01 +0700

    add injection for home interactor

--- MockaNews/Core/Injection/Injection.swift
+++ MockaNews/Core/Injection/Injection.swift
@@ -8,9 +8,14 @@
 import Foundation
 
 final class Injection: NSObject {
-    private func privateRepository() -> NewsRepositoryProtocol {
+    private func provideRepository() -> NewsRepositoryProtocol {
         let remote = RemoteDataSource.shared
         
         return NewsRepository.sharedInstance(remote)
+    }
+    
+    private func provideHome() -> HomeUseCase {
+        let repository = provideRepository()
+        return HomeInteractor(repository: repository)
     }
 }
