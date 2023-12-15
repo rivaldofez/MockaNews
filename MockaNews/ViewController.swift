f29649749614a354fe362304e6634b425906ce69
rivaldofez <rivaldofez@gmail.com>
2023-12-15 23:19:13 +0700

    setup base project and remove storyboard

---
+++ MockaNews/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MockaNews
+//
+//  Created by Rivaldo Fernandes on 15/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
+
