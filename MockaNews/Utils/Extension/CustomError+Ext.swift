c0bf111fd3d741a1a64362829107cd53df2542fb
rivaldofez <rivaldofez@gmail.com>
2023-12-15 23:35:53 +0700

    create custom error

---
+++ MockaNews/Utils/Extension/CustomError+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  CustomError+Ext.swift
+//  MockaNews
+//
+//  Created by Rivaldo Fernandes on 15/12/23.
+//
+
+import Foundation
+
+enum URLError: LocalizedError {
+    
+    case invalidResponse
+    case invalidURL
+    case addressUnreachable(URL)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidResponse: return "The server responded with garbage."
+        case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
+        case .invalidURL: return "URL is invalid and cannot be parse."
+        }
+    }
+}
