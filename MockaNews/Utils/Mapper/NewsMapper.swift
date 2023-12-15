283479fcf53fec653b7f5b24b45faf83b3f88435
rivaldofez <rivaldofez@gmail.com>
2023-12-16 00:02:11 +0700

    add mapper function to transform response model to domain model

---
+++ MockaNews/Utils/Mapper/NewsMapper.swift
@@ -0,0 +1,26 @@
+//
+//  NewsMapper.swift
+//  MockaNews
+//
+//  Created by Rivaldo Fernandes on 15/12/23.
+//
+
+import Foundation
+
+final class NewsMapper {
+    static func mapNewsResponsesToDomain(responses: [NewsResponse]) -> [News] {
+        return responses.map { mapNewsResponseToDomain(response: $0) }
+    }
+    
+    static func mapNewsResponseToDomain(response: NewsResponse) -> News {
+        return News(
+            id: response.id,
+            createdAt: response.createdAt,
+            contributorName: response.contributorName,
+            contributorAvatar: response.contributorAvatar,
+            title: response.title,
+            content: response.content,
+            contentThumbnail: response.contentThumbnail,
+            slideshow: response.slideshow)
+    }
+}
