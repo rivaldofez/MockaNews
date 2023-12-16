4eba1a863971f989a4742e19c1c5da2b37f7736a
rivaldofez <rivaldofez@gmail.com>
2023-12-16 16:29:36 +0700

    implement news image collection view cell

--- MockaNews/Module/Detail/View/NewsImageCollectionViewCell.swift
+++ MockaNews/Module/Detail/View/NewsImageCollectionViewCell.swift
@@ -13,23 +13,37 @@ class NewsImageCollectionViewCell: UICollectionViewCell {
     private let newsImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 8
+        imageView.layer.masksToBounds = true
         
         return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+    
         configureConstraints()
     }
     
     private func configureConstraints() {
         contentView.addSubview(newsImageView)
-        newsImageView.bounds = contentView.bounds
+        
+        let newsImageViewConstraint = [
+            newsImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 4),
+            newsImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -4),
+            newsImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            newsImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(newsImageViewConstraint)
+    }
+    
+    func configure(image: String) {
+        guard let url = URL(string: image) else { return }
+        newsImageView.sd_setImage(with: url)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
