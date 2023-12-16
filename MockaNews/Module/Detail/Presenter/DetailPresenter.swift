02f9ce09125c1b2aa9a547ab89a05f8861cba743
rivaldofez <rivaldofez@gmail.com>
2023-12-16 16:54:01 +0700

    add delegate function load load news data in detail view

--- MockaNews/Module/Detail/Presenter/DetailPresenter.swift
+++ MockaNews/Module/Detail/Presenter/DetailPresenter.swift
@@ -8,9 +8,12 @@
 import Foundation
 
 protocol DetailPresenterProtocol {
-    
+    var view: DetailViewProtocol? { get set }
+    var router: DetailRouterProtocol? { get set }
 }
 
 class DetailPresenter: DetailPresenterProtocol {
+    var view: DetailViewProtocol?
     
+    var router: DetailRouterProtocol?
 }
