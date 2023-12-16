ebd112efba6922484bcd9d8dcf47effcf467bc56
rivaldofez <rivaldofez@gmail.com>
2023-12-16 17:12:33 +0700

    implement router for detail view

--- MockaNews/Module/Detail/Router/DetailRouter.swift
+++ MockaNews/Module/Detail/Router/DetailRouter.swift
@@ -8,10 +8,29 @@
 import Foundation
 
 protocol DetailRouterProtocol {
+    var entry: DetailViewController? { get }
     
+    static func createDetail(with news: News) -> DetailRouterProtocol
 }
 
 
 class DetailRouter: DetailRouterProtocol {
+    var entry: DetailViewController?
+    
+    static func createDetail(with news: News) -> DetailRouterProtocol {
+        let router = DetailRouter()
+        
+        var view: DetailViewProtocol = DetailViewController()
+        var presenter: DetailPresenterProtocol = DetailPresenter()
+        
+        view.presenter = presenter
+        presenter.router = router
+        presenter.view = view
+        presenter.setNewsData(news: news)
+        
+        router.entry = view as? DetailViewController
+        return router
+    }
+    
     
 }
